68fb72674ab5c73be1c63f5238efa27f564865b1
Kellin Baldenegro <kellinjoshua@gmail.com>
2019-05-22 14:46:08 -0700

    develope file structure

--- db/schema.sql
+++ db/schema.sql
@@ -3,17 +3,8 @@ CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(128) NOT NULL,
     devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 );
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("The Ultimate Burger", TRUE);
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("The Bacon Burger", FALSE);
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("The Cheesy Burger", TRUE);
